9abacd6e98903c0a83c6107494044d06c375cfa5
sumin <proysm5154@naver.com>
2023-04-09 20:02:42 +0900

    [Solved] 그리디

---
+++ BOJ/그리디/11047/code.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[12];
+int ans=0;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N, K;
+
+    cin >> N >> K;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i=N-1; i>=0; i--){
+        ans += K / arr[i];
+        K %= arr[i];
+    }
+    cout << ans;
+    
+    return 0;
+}
