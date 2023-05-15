d7e77b62ade9543a1b36be757033638e19e46793
sumin <proysm5154@naver.com>
2023-05-15 16:36:47 +0900

    [insert] 문제 추가

---
+++ BOJ/BFS/7576 토마토/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int board[55][55];
+int vis[55][55];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    return 0;
+}
