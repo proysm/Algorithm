46121f33be149619a949d802102c730e30be01cc
유수민 <proysm5154@naver.com>
2023-08-10 01:10:01 +0900

    Solve 해시 7785

--- BOJ/해시/7785 회사에 있는 사람/code.cpp
+++ BOJ/해시/7785 회사에 있는 사람/code.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    unordered_set<string> us;
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        string name, cmd;
+        cin >> name >> cmd;
+        if(cmd == "enter") us.insert(name);
+        else if(cmd == "leave") us.erase(name);
+    }
+    vector<string> ans(us.begin(), us.end());
+    sort(ans.begin(), ans.end(), greater<string>());
+    for(auto x : ans) cout << x << '\n';
     
 }
